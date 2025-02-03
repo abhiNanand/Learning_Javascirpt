5f71a9e5344d733b82d97127c924bd7b22597c92
Pooja <pooja@syn-192-180-000-017.res.spectrum.com>
2025-02-03 15:39:25 +0530

    uploading

--- 19.Events/task schedular/mainfile/todo.js
+++ 19.Events/task schedular/mainfile/todo.js
@@ -1,31 +1,18 @@
-let todolist = [
+let todolist = [];
 
  
-
-];
-
- 
-
-
-
 const clickEnter = document.querySelector('#text');
 clickEnter.addEventListener("keypress",
 
-
-  function (event) {
+function (event) {
     if (event.key === 'Enter') {
-      event.preventDefault();//agar ye use nhi karenge too sahi se kaam nhi karta
+      event.preventDefault();//agar ye use nhi karenge too sahi se kaam nhi karta. page ko refresh hone se rokta hai
       addTodo();
     }
   }
 );
 
-
-
-
-
-displayItems();
-
+showComplete(2);
 
 function addTodo() {
   const input = document.querySelector('#text');
@@ -41,17 +28,17 @@ function addTodo() {
   todolist.push({task:itemList,c:0});
 
   input.value = '';
-  displayItems();
+  showComplete(2);
 }
 
 
 function checkFunction(index, id) {
 
   const checkbox = document.querySelector(`#${id}`);  //if i use ("#id") there will be error. because id will go as string not value.
-   
+    
   if (checkbox.checked == true) {
-     todolist[index].c=1;
-     console.log(todolist[index].c);
+    todolist[index].c=1;
+    console.log(todolist[index].c);
   }
   else {
     todolist[index].c=0;
@@ -83,57 +70,37 @@ taskStatus.addEventListener("change", function() {
 function showComplete(flag)
 {
 
-        let newhtml='';
-        const show = document.querySelector('.showTask');
-  for (let i = 0; i < todolist.length; i++) {
+let newhtml='';
+const show = document.querySelector('.showTask');
+for (let i = 0; i < todolist.length; i++) {
 
+ let p="";
 
-
-  let p="";
-  if(flag==1)
-   p="checked";
- else if(flag==0)
-   p="";
- else
- {
-    if(todolist[i].c==0)
-      p="";
-    else
-    p="checked"; 
- }
+  if(todolist[i].c==0)
+    p="";
+  else
+  p="checked"; 
+ 
 
     let items = todolist[i].c;
     
     if(items ==flag || flag==2){
     newhtml += `<br>  <input type="checkbox" id="check${i}"  onClick="checkFunction( ${i} ,id)" ${p}>   ${todolist[i].task}
-     <button class='btn-delete' onclick="todolist.splice(${i}, 1);
-       displayItems(todolist);"> <i class="material-icons">delete</i> </button>
+     <button class='btn-delete' onclick="deleteItems('check${i}',${i},${flag});"> <i class="material-icons">delete</i> </button>
     `
     }
   }
   show.innerHTML = newhtml;
-
 }
 
- 
-
-
-function displayItems() {
-  let newhtml = '';
-  const show = document.querySelector('.showTask');
-  for (let i = 0; i < todolist.length; i++) {
-    let items = todolist[i].task;
-    
-    newhtml += `<br>  <input type="checkbox" id="check${i}"  onClick="checkFunction( ${i} ,id)" >   ${items}
-     <button class='btn-delete' onclick="todolist.splice(${i}, 1);
-       displayItems(todolist);"> <i class="material-icons">delete</i> </button>
-    `
-  }
-  show.innerHTML = newhtml;
-
+//get id as string not object
+function deleteItems(id,index,flag)
+{
+const checkbox=document.querySelector(`#${id}`);
+if(checkbox.checked != true )
+  todolist.splice(index,1);
+  showComplete(flag);
 }
-
-
 
 
 
---
+++ 19.Events/task schedular/mainfile/todolocalStorage.js
@@ -0,0 +1,168 @@
+
+//initilizing todo list from local storage.
+let todolist = JSON.parse(localStorage.getItem('todolist')) || [];
+
+
+//save tasks in localstorage
+
+function saveTask()
+{
+    localStorage.setItem('todolist',JSON.stringify(todolist));
+}
+
+
+//getting task from local storage
+
+ 
+
+//add task when pressed enter.
+const clickEnter = document.querySelector('#text');
+
+//adding event listener to enter key
+clickEnter.addEventListener('keypress',function (event) {
+    if (event.key === 'Enter') {
+      event.preventDefault();//agar ye use nhi karenge too sahi se kaam nhi karta. page ko refresh hone se rokta hai
+      addTodo();
+    }
+  }
+);
+
+ 
+
+
+
+//function to add task
+function addTodo() {
+    const input = document.querySelector('#text');
+    const itemList = input.value;
+    if (itemList.trim() == '')      //trim removes white space
+    {
+  
+      alert('Task cannot be empty');
+      return;
+  
+    }
+  
+    todolist.push({task:itemList,c:'pending'});
+  
+    input.value = '';
+    saveTask(); //saving task to local storage
+    showTask('all');
+  }
+  
+
+  //handling checkbox
+
+function checkFunction(index, id,taskType) {
+
+    let taskstatus = document.querySelector(`#${id}`);  //if i use ("#id") there will be error. because id will go as string not value.
+
+   
+     
+
+    if (taskstatus .getAttribute('value') == 'pending') {
+         
+      todolist[index].c='completed';
+      taskstatus.setAttribute('value','complete');
+    }
+    else {
+      todolist[index].c='pending';
+      taskstatus.setAttribute('value','pending');
+    }
+    saveTask();   //updating in localstorage  
+ if(taskType != 'all')
+ {
+    showTask(taskType);
+ }
+
+
+   
+  }
+
+
+ //selecting completed/uncompleted/all task
+ const taskStatus = document.querySelector("#taskStatus");
+ taskStatus.addEventListener("change", function() {
+   const selectedValue = this.value;  
+       showTask(selectedValue);
+ });
+
+//function to delete task
+function deleteItems(id,todo_list_index,taskType)
+{
+const checkbox=document.querySelector(`#${id}`);
+if(checkbox.checked != true )
+  todolist.splice(todo_list_index,1);
+
+ 
+  saveTask();
+  showTask(taskType);
+}
+
+//showing function on page
+function showTask(taskType)
+{
+
+let countTask=0;
+let newhtml='';
+const inputfiled = document.querySelector('#text');
+const buttonaction= document.querySelector('.btn1');
+
+
+ if(taskType != 'all')
+ {
+        
+         inputfiled.disabled = true;
+         buttonaction.disabled=true;
+
+ }
+
+ else
+ {
+
+
+    inputfiled.disabled = false;
+    buttonaction.disabled=false;
+
+ }
+ 
+  
+
+const show = document.querySelector('.showTask');
+for (let i = 0; i < todolist.length; i++) {
+
+ let p="";
+ if(todolist[i].c =='pending')
+  p="";
+  else
+  p="checked"; 
+
+
+    let itemsStatus = todolist[i].c;
+    
+    if(itemsStatus == taskType || taskType == 'all'){
+        let k = `<h1> hello </h1>`
+    countTask++;
+    newhtml += `<br>`+`k`+ ` <input type="button" value="submit" id="check${i}"  onClick="checkFunction( ${i} ,id,'${taskType}')" ${p}>   ${todolist[i].task}
+     <button class='btn-delete' onclick="deleteItems('check${i}',${i},'${taskType}');"> <i class="material-icons">delete</i> </button>
+    ` 
+
+
+    }
+  }
+
+
+if( countTask == 0)
+{
+    console.log('a')
+    newhtml = `<h1> no task available </h1>`
+}
+
+  show.innerHTML = newhtml;
+}
+
+
+ 
+showTask('all');
+
+
